4f4006dd5417b452d1ce8899184a17ef5a804eb6
itsumarashraf <itsumarashraf@gmail.com>
2024-05-04 11:38:08 +0530

    comps added

---
+++ src/app/timing/timing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timing',
+  standalone: true,
+  imports: [],
+  templateUrl: './timing.component.html',
+  styleUrl: './timing.component.scss'
+})
+export class TimingComponent {
+
+}
